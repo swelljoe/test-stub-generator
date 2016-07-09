369d5000d7ecb3d8c01721ca9d4e0e0368cd5061
Joe Cooper <joe@virtualmin.com>
2016-07-08 20:32:59 -0700

    kinda works now

--- stubby.pl
+++ stubby.pl
@@ -19,8 +19,6 @@ unless (-d "./t") {
 foreach my $file (@files) {
   my $shortname = $file;
   $shortname =~ s{\.[^.]+$}{};
-  say $shortname;
-  say $file;
 
   # Create a strict/warnings syntax test for each file
   unless (-e "./t/$shortname-syntax.t") {
@@ -41,6 +39,8 @@ EOF
   # or defined (our definitions).
   say "Working on $file";
   my %globals;
+  my %config;
+  my %in;
   open my $fh, "<", $file;
   while (<$fh>) {
     if (m/^our (?<global>.+);/) {
@@ -54,28 +54,42 @@ EOF
       $globals{$+{global}}++;
     }
     # Collect all config variables referenced
-    my %config;
     m/\$config\{'(?<config>.+?)'\}/ && $config{$+{config}}++;
     # Collect %in variables referenced
-    my %in;
     m/\$in\{'(?<in>.+?)'\}/ && $in{$+{in}}++;
-    #
+  }
+
+  unless (-e "t/$shortname.t") {
+    # Spit them into comments in a new test file
+    open my $test, ">", "t/$shortname.t";
+    print $test <<"EOF";
+use strict;
+use warnings;
+use v5.10.1;
+
+EOF
+    if (scalar keys %globals) {
+      print $test "# Global variables pulled from web-lib and module lib\n";
+      foreach my $key (keys %globals) {
+        print $test "my $key;\n";
+      }
+    }
+    if (scalar keys %config) {
+      print $test "# Config variables\n";
+      print $test "my %config;\n";
+      foreach my $key (keys %config) {
+        # Dummy values, need to change during test construction
+        print $test "\$config{'$key'} = 0;\n"
+      }
+    }
+    if (scalar keys %in) {
+      print $test "# Input values (query string or posted values)\n";
+      print $test 'my %in\n';
+      foreach my $key (keys %in) {
+        # Dummy values, change'em when writing tests
+        print $test "\$in{'$key'} = 0;\n"
+      }
+    }
   }
   close $fh;
-  say Dumper(keys %globals);
-
-  #foreach my $global (keys %globalsall) {
-  #  # Split out parenthesized list of vars
-  #my @is_list;
-  #  if ($global =~ m/^\((?<is_list>.+)\)/) {
-  #    say "In is_list";
-  #    push (@is_list, (split /, /, $+{is_list}));
-  #    delete $globalsall($_);
-  #  }
-  #  print Dumper(@is_list);
-  #}
 }
-# Create mocks for globals vars
-
-# Get the list of functions in this module, and generate a stub test file for
-# each one, if one doesn't already exist.
